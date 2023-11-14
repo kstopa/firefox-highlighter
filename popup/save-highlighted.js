69332fa5c187c8a5f721d5088546229df94bd1c6
Krzysztof Stopa <k.stopa86@gmail.com>
2023-11-14 23:10:53 +0100

    feat: enable to save to a file highlighted content

--- popup/save-highlighted.js
+++ popup/save-highlighted.js
@@ -1,5 +1,32 @@
+function saveTextAsFile() {
+    var textToWrite = document.getElementById('highlightedContentArea').value;
+    var textFileAsBlob = new Blob([ textToWrite ], { type: 'text/plain' });
+    var fileNameToSaveAs = "highlighted.md";
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null) {
+        // Chrome allows the link to be clicked without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    } else {
+        // Firefox requires the link to be added to the DOM before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+    downloadLink.click();
+}
+
+function destroyClickedElement(event) {
+    // remove the link from the DOM
+    document.body.removeChild(event.target);
+}
+
+
 function renderMarkdown(content, activeTab) {
-    document.getElementById("highlightedContent").value = `#${activeTab.title} \n\n${content}\n\nSource: ${activeTab.url}`;
+    document.getElementById("highlightedContentArea").value = `# ${activeTab.title} \n\n${content}\n\nSource: ${activeTab.url}`;
 }
 
 browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -11,3 +38,5 @@ browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
         renderMarkdown(content.highlightedContent, activeTab);
     });
 });
+
+document.getElementById('save').addEventListener('click', saveTextAsFile);
